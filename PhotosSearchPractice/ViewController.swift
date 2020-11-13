d6a1fb7d8ed8d6ffa5d44a012a03cc470d77cfbb
潘立祥 <salt918077@gmail.com>
2020-11-13 23:33:13 +0800

    [Setup]: 套用 git ignore

---
+++ PhotosSearchPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosSearchPractice
+//
+//  Created by 潘立祥 on 2020/11/13.
+//  Copyright © 2020 PanLiHsiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotosSearchPracticeTests/PhotosSearchPracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  PhotosSearchPracticeTests.swift
+//  PhotosSearchPracticeTests
+//
+//  Created by 潘立祥 on 2020/11/13.
+//  Copyright © 2020 PanLiHsiang. All rights reserved.
+//
+
+import XCTest
+@testable import PhotosSearchPractice
+
+class PhotosSearchPracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
