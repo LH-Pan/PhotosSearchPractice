b6a2e246ca1473b897ef6ebc015178882ac17302
潘立祥 <salt918077@gmail.com>
2020-11-14 12:46:14 +0800

    [Add]: 將 Main.storyboard 刪除，改用純 code 建置 window。 iOS 13 window 生成方式與版本 13 以下不同，故加入判斷版本以及符合當前版本的 window 建置方式。現在 window 的 rootViewController 為 Navigation Controller，於進入第一個畫面時將 navigation bar 隱藏

---
+++ PhotosSearchPractice/View/ViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+}
+
