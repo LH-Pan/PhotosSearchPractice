40c638f58815989eb9e7fc167bf54b55043df91e
潘立祥 <salt918077@gmail.com>
2020-11-16 11:11:48 +0800

    [Refactor]: 將 setupNavigationController 中所有參數改寫入 enum，讓 tag 用 enum 的 rawValue，其他則使用 computer property，維護或擴充較為便利一些

--- PhotosSearchPractice/View/PSTabBarController.swift
+++ PhotosSearchPractice/View/PSTabBarController.swift
@@ -1,9 +1,41 @@
 import UIKit
 
 class PSTabBarController: UITabBarController {
-
-    // MARK: - Private Constant / Variable Declare
-    private var tag: Int = 0
+    
+    enum Navigation: Int {
+        
+        case featured = 0
+        
+        case favorite = 1
+        
+        var rootVC: UIViewController {
+            
+            switch self {
+                
+            case .featured: return SearchViewController()
+                
+            case .favorite: return FavoriteViewController()
+            }
+        }
+        
+        var tabBarTitle: String {
+            
+            switch self {
+                
+            case .featured: return "Featured"
+                
+            case .favorite: return "Favorite"
+            }
+        }
+        
+        var tabBarImageName: String {
+            
+            switch self {
+                
+            case .featured, .favorite: return "star.fill"
+            }
+        }
+    }
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
@@ -15,30 +47,22 @@ class PSTabBarController: UITabBarController {
     // MARK: - Private Method
     private func setupTabBarViewControllers() {
         
-        let featuredNavigationController = setupNavigationController(rootVC: SearchViewController(),
-                                                                     tabBarTitle: "Featured")
-        
-        let favoriteNavigationController = setupNavigationController(rootVC: FavoriteViewController(),
-                                                                     tabBarTitle: "Favorite")
-        
-        self.viewControllers = [featuredNavigationController, favoriteNavigationController]
+        self.viewControllers = [setupNavigationController(navigation: .featured),
+                                setupNavigationController(navigation: .favorite)]
     }
     
-    private func setupNavigationController(rootVC: UIViewController,
-                                           tabBarTitle: String,
-                                           tabBarImageName: String = "star.fill"
-                                           ) -> UINavigationController {
-        
-        let viewController = rootVC
-        
+    private func setupNavigationController(navigation: Navigation) -> UINavigationController {
+
+        let viewController = navigation.rootVC
+
         let navigationController = PSNavigationController(rootViewController: viewController)
-        
-        let item = UITabBarItem(title: tabBarTitle, image: UIImage(systemName: tabBarImageName), tag: tag)
-        
+
+        let item = UITabBarItem(title: navigation.tabBarTitle,
+                                image: UIImage(systemName: navigation.tabBarImageName),
+                                tag: navigation.rawValue)
+
         navigationController.tabBarItem = item
-        
-        tag += 1
-        
+
         return navigationController
     }
 }
